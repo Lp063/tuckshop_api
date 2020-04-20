f647f526f30d186c3876831582bdf7300902444c
Lohit <lohit.pereira09@stanislausbandra.in>
2020-04-20 22:18:44 +0530

    jsonwebtokens implemented

--- index.js
+++ index.js
@@ -1,33 +1,90 @@
-var users       =   require('./model/users');
 const express   =   require('express');
-//var cors        =   require('cors');
+const jwt       =   require('jsonwebtoken');
+var cors        =   require('cors');
 const app       =   express();
 var bodyParser  =   require('body-parser');
 
-//app.use(cors())
+app.use(cors())
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.get('/',(req,res)=>{
+var users       =   require('./model/users');
+var teams       =   require('./model/teams');
+
+
+app.post('/api/login',(req,res)=>{
+    var data={
+        email:req.body.loginid,
+        password:req.body.loginpass
+    };
+    response = users.authentication(data,function(err,data){
+        res.setHeader('Content-Type', 'application/json');
+        const thisUser = data[0];
+        jwt.sign({userData:thisUser},'kamakazi',(err,token)=>{
+            res.json({token});
+        });
+    });
+});
+
+//Users API
+app.get('/api/getAllUsers',(req,res)=>{
     response = users.getAllUsers(function(err,data){
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(data));  
     });
 });
 
-app.post('/addUsers',(req,res)=>{
-    /* var insertObject = {
-            "name":"Tanmay",
-            "username":"tanmayb",
-            "email":"tanmayb@unboxsocial.com",
-            "password":"5787543444"
-        }; */
-    
+app.get('/api/uniqueEmailCheck',(req,res)=>{
+    response = users.uniqueEmailValidator(req.body,function(err,data){
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data));  
+    });
+});
+
+app.post('/api/addUsers',(req,res)=>{
+    /* {
+        "name":"Tanmay",
+        "username":"tanmayb",
+        "email":"tanmayb@unboxsocial.com",
+        "password":"5787543444"
+    }; */
     response = users.addUsers(req.body,function(err,data){
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(data));  
     });
 });
+
+//team API
+app.post('/api/createTeam', verifyToken,(req,res)=>{
+    jwt.verify(req.token,'kamakazi',(error, authData)=>{
+        if (error) {
+            res.sendStatus(403);
+        } else {
+            res.json({
+                authData,
+                message:" team Created"
+            });
+        }
+    });
+    /* response = users.createTeam(req.body,function(err,data){
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(data));  
+    }); */
+});
+
+
+function verifyToken(req, res, next){
+    const bearerHeader = req.headers['authorization'];
+
+    if(typeof bearerHeader == "undefined"){
+        res.sendStatus(403);
+    }else{
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    }
+}
 
 app.listen(4000,()=>{
     console.log("listening on port 4000");
--- model/users.js
+++ model/users.js
@@ -1,6 +1,15 @@
 var config = require('../config/config');
 
 
+module.exports.authentication=(input,callback)=>{
+    var query='SELECT * FROM `users` WHERE `email` ='+config.mysqlConnection.escape(input.email)+' and `password` ='+config.mysqlConnection.escape(input.password)+' ';
+    config.mysqlConnection.connect();
+    var query = config.mysqlConnection.query(query,function(error, results, fields){
+        callback(null,results);
+    });
+    config.mysqlConnection.end();
+}
+
 module.exports.getAllUsers=(callback)=>{
     var rows=[];
     config.mysqlConnection.connect();
@@ -19,6 +28,15 @@ module.exports.addUsers=(insertObject,callback)=>{
     var query='insert into `users` SET ?';
     config.mysqlConnection.connect();
     var query = config.mysqlConnection.query(query,insertObject,function(error, results, fields){
+        callback(null,results);
+    });
+    config.mysqlConnection.end();
+}
+
+module.exports.uniqueEmailValidator=(input,callback)=>{
+    var query='SELECT COUNT(*) as count FROM `users` WHERE `email` like '+config.mysqlConnection.escape(input.email);
+    config.mysqlConnection.connect();
+    var query = config.mysqlConnection.query(query,function(error, results, fields){
         callback(null,results);
     });
     config.mysqlConnection.end();
